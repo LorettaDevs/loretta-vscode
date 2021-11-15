2401053c78fc35e72dc10762c97370a7499e4e0d
GGG KILLER <gggkiller2@gmail.com>
2021-11-15 07:24:28 -0300

    Update the publish server script.

--- publish-server.ps1
+++ publish-server.ps1
@@ -1,7 +1,15 @@
+param(
+	[switch]$Debug = $false
+)
 Push-Location "server"
 Remove-Item -Path "bin" -Recurse
 foreach ($os in "win", "linux", "osx") {
-	dotnet publish --nologo --verbosity quiet --self-contained --framework net6.0 -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime $os-x64 --output bin/tmp
+	if ($Debug) {
+		dotnet publish --nologo --verbosity quiet --self-contained --configuration Debug --framework net6.0 -p:PublishSingleFile=true --runtime $os-x64 --output bin/tmp
+	}
+	else {
+		dotnet publish --nologo --verbosity quiet --self-contained --configuration Release --framework net6.0 -p:PublishSingleFile=true -p:PublishTrimmed=true --runtime $os-x64 --output bin/tmp
+	}
 	if ($os -eq "win") {
 		Move-Item -Path "bin/tmp/Loretta.LanguageServer.exe" -Destination "bin/loretta-lsp-win.exe"
 	}
